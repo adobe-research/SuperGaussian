a1863fd4d473274441d599a30cca693c7ff2fd50
Yuan Shen <yshen47@illinois.edu>
2024-09-08 16:37:26 -0500

    updating readme.md

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
-# SuperGaussian_ECCV24
+# SuperGaussian: Repurposing Video Models for 3D Super Resolution
 
-Official codebase for SuperGaussian accepted to ECCV 2024. 
+Yuan Shen, Duygu Ceylan, Paul Guerrero, Zexiang Xu, Niloy J. Mitra, Shenlong Wang, and Anna Frühstück
 
-### Installment
-```
+ECCV 2024
+
+[Paper](https://arxiv.org/abs/2406.00609) │ [Project Page](https://supergaussian.github.io/)
+
+TLDR: Instead of image prior, we use video upsampling prior to achieve 3D upsampling on generic low-res 3D representations. 
+
+In this codebase, we provide a SuperGaussian implementation on a third-parties video upsampling prior, [RealBasicVSR](https://github.com/ckkelvinchan/RealBasicVSR). 
+Additionally, our codebase provides our 3D upsampling evaluations on the MVImgNet test set given the upsampled image sequence from the image prior GigaGAN (image) and video prior Videogigagan (video).
+Hopefully, this could help the community to reproduce our results and compare with other methods.
+
+### Dependencies
+
+#### 1. Install with Conda
+```bash
 conda create -n super_gaussian_eccv24 python=3.8 -y
 
 conda activate super_gaussian_eccv24
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cu118
+
 conda install cuda -c nvidia/label/cuda-11.8.0 -y
 pip install tqdm plyfile==0.8.1 hydra-core==1.3.1 h5py==3.8.0 autolab_core==1.1.1 pyiqa timm==0.9.10 rich wandb lpips boto3 tyro
 export CUDA_HOME=$(dirname $(dirname $(which python)))
@@ -26,6 +39,48 @@ pip install mmedit==0.15.0
 conda activate super_gaussian_eccv24
 ```
 
-### Inference
+#### 2. Install with Docker
+```bash
+docker pull yshen47/adobe_supergaussian:latest
+docker run -it -v $(DATA_PATH):/mnt/data --shm-size=64g --gpus all yshen47/adobe_supergaussian:latest bash
+cd /root
+cd SuperGaussian_ECCV24
+ln -s /mnt/data data
+```
 
-Run inference on MVImgNet testset with a third-party VSR prior, RealBasicVSR. Note, due to license issue, we cannot release one with VideoGigaGAN or GigaGAN checkpoints. 
+### Test data Download
+
+1. Download the MVImgNet test set with 523 scenes. Each scene are grouped first by its category id and then scene id, defined by MVImgNet.
+The test scenes are selected for its rich details, diversity and challenging scenarios. Within each scene folder, the directory are organized as follows:
+```     
+--- [category_id]/[scene_id]
+    --- cam_extrinsics.pkl
+    --- cam_intrinsics.pkl
+    --- rgb.pkl
+    --- xyz.pkl
+    --- novel_trajectory_poses
+        --- 0000.json
+        --- 0001.json
+        ...
+    --- gt_rgb
+        --- 0000.png
+        --- 0001.png
+        ...
+    --- LR_131072_gaussian
+        --- 0000.png
+        --- 0001.png
+        ...
+    --- HR_131072_gaussian
+        --- 0000.png
+        --- 0001.png
+        ...
+        
+    --- gigagan_image
+        --- 0000.png
+        --- 0001.png
+        ...
+    ... upsampled_rgb_videogigagan
+        --- 0000.png
+        --- 0001.png
+        ...
+```bash
---
+++ evaluation.py
@@ -0,0 +1,177 @@
+import random
+import json
+import os
+import pickle
+from sg_utils.colmap_utils import read_extrinsics_binary
+import torchvision.transforms as transforms
+import PIL
+import multiprocessing
+import pyiqa
+from PIL import Image
+import numpy as np
+import torch
+from pathlib import Path
+import time
+from tqdm import tqdm
+from torchmetrics.image.inception import InceptionScore
+
+
+num_workers = 16
+
+def worker(
+    queue: multiprocessing.JoinableQueue,
+    count: multiprocessing.Value,
+) -> None:
+    while True:
+        scene_dir  = queue.get()
+        if scene_dir == 'Done':
+            break
+        get_images(scene_dir)
+        with count.get_lock():
+            count.value += 1
+
+        queue.task_done()
+
+
+def get_images(scene_dir):
+    print(dataset_root, scene_dir)
+    cameras_extrinsic_file = os.path.join(dataset_root, "mvimgnet_testset_500", scene_dir, "cam_extrinsics.pkl")
+    with open(cameras_extrinsic_file, 'rb') as f:
+        cam_extrinsics = pickle.load(f)
+    cam_infos = sorted(cam_extrinsics.values(), key=lambda x: x.name)
+    for cam_info in list(cam_infos):
+        filename = f"traj_0_{(int(cam_info.name[:-4])-1):03d}.png"
+        image_path = os.path.join(
+            dataset_root, "mvimgnet_testset_500", scene_dir, "HR_131072_gaussian", filename
+        )
+
+        image = Image.open(image_path)
+        # image = transforms.CenterCrop(min(image.size))(image)
+        gt_image = image.resize(
+            (256, 256), resample=PIL.Image.BILINEAR
+        )
+        name = scene_dir.replace('/', '_') + '_' + cam_info.name[:-4] + '.png'
+        try:
+            if os.path.exists(os.path.join(gt_cache_path, name)):
+                Image.open(os.path.join(gt_cache_path, name))
+            else:
+                gt_image.save(os.path.join(gt_cache_path, name))
+        except:
+            gt_image.save(os.path.join(gt_cache_path, name))
+
+        # load pred
+        # filename = f"{(int(cam_info.name[:-4]) - 1):03d}.png"
+        image_path = os.path.join(
+            target_root, scene_dir, f"super_gaussian_with_{target}/step_2_fitting_with_3dgs/point_cloud/iteration_2000/predicted", filename
+        )
+        image = Image.open(image_path)
+
+        try:
+            if os.path.exists(os.path.join(pred_cache_path, name)):
+                Image.open(os.path.join(pred_cache_path, name))
+            else:
+                image.save(os.path.join(pred_cache_path, name))
+        except:
+            image.save(os.path.join(pred_cache_path, name))
+
+        image_path = os.path.join(
+            dataset_root, "mvimgnet_testset_500", scene_dir, "gt_rgb", cam_info.name[:-4] + '.png'
+        )
+
+        image = Image.open(image_path)
+
+        image = transforms.CenterCrop(min(image.size))(image)
+        gt_image = image.resize(
+            (256, 256), resample=PIL.Image.BILINEAR
+        )
+        name = scene_dir.replace('/', '_') + '_' + cam_info.name[:-4] + '.png'
+
+        try:
+            if os.path.exists(os.path.join(fid_gt_cache_path, name)):
+                Image.open(os.path.join(fid_gt_cache_path, name))
+            else:
+                gt_image.save(os.path.join(fid_gt_cache_path, name))
+        except:
+            gt_image.save(os.path.join(fid_gt_cache_path, name))
+
+
+if __name__ == '__main__':
+    target = 'realbasicvsr'
+
+    queue = multiprocessing.JoinableQueue()
+    count = multiprocessing.Value("i", 0)
+    dataset_root = '/home/yuan/PycharmProjects/SuperGaussian_ECCV24/data'
+    target_root = f'/media/yuan/T7_citygen/supergaussian/{target}_test_results'
+    fid_gt_cache_path = f'/media/yuan/T7_citygen/supergaussian/evaluations/{target}_prior/gt_fid'
+    gt_cache_path = f'/media/yuan/T7_citygen/supergaussian/evaluations/{target}_prior/gt'
+    pred_cache_path = f'/media/yuan/T7_citygen/supergaussian/evaluations/{target}_prior/pred'
+    os.makedirs(gt_cache_path,exist_ok=True)
+    os.makedirs(pred_cache_path,exist_ok=True)
+    os.makedirs(fid_gt_cache_path,exist_ok=True)
+
+    with open(f'{dataset_root}/mv_imagenet_category.txt', 'r') as f:
+        category_mapping = {}
+        for line in f.readlines():
+            idx, category = line.strip().split(',')
+            category_mapping[category.strip()] = int(idx.strip())
+
+    split_scene_dirs = []
+    categories = [str(elem) for elem in list(sorted(category_mapping.values()))]
+
+
+    with open(f'{dataset_root}/supergaussian_testset.json', 'r') as f:
+        split_scene_dirs = json.load(f)
+
+    for worker_i in range(num_workers):
+        process = multiprocessing.Process(
+            target=worker, args=(queue, count,)
+        )
+        process.daemon = True
+        process.start()
+
+    for scene_dir in tqdm(split_scene_dirs):
+        queue.put((scene_dir))
+
+    while True:
+        time.sleep(5)
+        print(f"Finished {count.value}/{len(split_scene_dirs)}")
+        if count.value == len(split_scene_dirs):
+            break
+
+    # Wait for all tasks to be completed
+    queue.join()
+    count.value = 0
+    # Add sentinels to the queue to stop the worker processes
+    for i in range(num_workers):
+        queue.put('Done')
+
+
+    fid_metric = pyiqa.create_metric('lpips')
+
+    scores = []
+    for fn in tqdm(Path(gt_cache_path).glob('*.png'), total=len(list(Path(gt_cache_path).glob('*.png')))):
+        score = fid_metric(str(fn), str(fn).replace('gt', 'pred'))
+        scores.append(score)
+    print(f'{target} LPIPS: {torch.stack(scores).mean()}')
+
+
+    fid_metric = pyiqa.create_metric('niqe')
+
+    scores = []
+    for fn in tqdm(Path(pred_cache_path).glob('*.png'), total=len(list(Path(pred_cache_path).glob('*.png')))):
+        score = fid_metric(str(fn))
+        scores.append(score)
+    print(f'{target} NIQE: {torch.stack(scores).mean()}')
+
+
+    inception = InceptionScore()
+    img_list = list(sorted(Path(pred_cache_path).glob('*.png')))
+    import random
+    random.seed(0)
+    img_list = random.sample(img_list, 1000)
+    for pred_path in tqdm(img_list, total=len(img_list)):
+        img = torch.tensor(np.array(Image.open(str(pred_path)))).permute(2, 0, 1)[None, ]
+        inception.update(img)
+    print("IS: ", inception.compute())
+    fid_metric = pyiqa.create_metric('fid')
+    print("FID: ", fid_metric(fid_gt_cache_path, pred_cache_path))
